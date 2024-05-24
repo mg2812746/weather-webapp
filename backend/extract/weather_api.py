06880d537774b6268b661991ed0c0a0feea286cd
Miguel Galvez <m.galvez.dev@gmail.com>
2024-05-23 22:03:51 -0700

    update weather_api.py
    successful request

--- backend/extract/weather_api.py
+++ backend/extract/weather_api.py
@@ -9,18 +9,22 @@ class WeatherAPI:
 
     def __str__(self):
         return f"{self.city} {self.state}"
-    # to be completed
-    def get_weather_data(self) -> json:
-        data = {}
-        data = json.dumps(data)
-        
-        return data
     # return latitude, longitude from city and state
     def geocode(self) -> list:
         geolocator = Nominatim(user_agent="weather visualizer")
         g = geolocator.geocode(f'{self.city}, {self.state}')
         coords = [g.latitude, g.longitude]
         return coords
+    # to be completed
+    def get_weather_data(self) -> json:
+        coords = self.geocode()
+        r = requests.get(f"https://api.weather.gov/points/{coords[0]},{coords[1]}")
+        # Add timestamp of data accessed
+        data = r.json()
+        return data
+    
     
 # driver
 testAPI = WeatherAPI('Fontana', 'CA')
+data = testAPI.get_weather_data()
+print(data)
