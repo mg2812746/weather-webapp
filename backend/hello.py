2db014d4a8e9eec399d79c3c29447eb3399fda91
Miguel Galvez <m.galvez.dev@gmail.com>
2024-06-27 04:20:45 -0700

    submit city and state data successful query

--- backend/hello.py
+++ backend/hello.py
@@ -1,7 +1,25 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+app.debug = True
+
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+@app.route('/read-form', methods=['POST'])
+def read_form():
+
+    # Get form data as dict
+    data = request.form
+
+    ## Return the extracted information
+    return {
+        'city': data['city'],
+        'state': data['state'],
+    }
+
+if __name__ == '__main__':
+    # Run the app on local development server
+    app.run()
